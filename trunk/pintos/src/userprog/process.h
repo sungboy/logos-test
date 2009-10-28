71e981adb4837f7230c54f4c3c4acabd03359fa0
dmpark04 <dmpark04@2f210a3a-a0f4-11de-923e-0b953e4d95d6>
2009-10-28 15:35:42 +0000

    유저 영역 가상 메모리 주소 확인 함수 구현

--- trunk/pintos/src/userprog/process.h
+++ trunk/pintos/src/userprog/process.h
@@ -8,4 +8,6 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
+bool process_is_valid_user_virtual_address (const void *uvaddr, size_t size, bool writable);
+
 #endif /* userprog/process.h */
