c1b727ca8e3127511f9a3a19a350b4188713f2f6
dmpark04 <dmpark04@2f210a3a-a0f4-11de-923e-0b953e4d95d6>
2009-10-30 16:19:24 +0000

    file system 관련 일부 락 추가(free_map_* 함수)

--- trunk/pintos/src/filesys/free-map.c
+++ trunk/pintos/src/filesys/free-map.c
@@ -4,14 +4,20 @@
 #include "filesys/file.h"
 #include "filesys/filesys.h"
 #include "filesys/inode.h"
+#include "threads/synch.h"
 
 static struct file *free_map_file;   /* Free map file. */
 static struct bitmap *free_map;      /* Free map, one bit per disk sector. */
+
+/* LOGOS-ADDED VARIABLE */
+struct lock free_map_lock;
 
 /* Initializes the free map. */
 void
 free_map_init (void) 
 {
+  lock_init (&free_map_lock);
+
   free_map = bitmap_create (disk_size (filesys_disk));
   if (free_map == NULL)
     PANIC ("bitmap creation failed--disk is too large");
@@ -26,6 +32,8 @@ free_map_init (void)
 bool
 free_map_allocate (size_t cnt, disk_sector_t *sectorp) 
 {
+  lock_acquire (&free_map_lock);
+
   disk_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
   if (sector != BITMAP_ERROR
       && free_map_file != NULL
@@ -36,6 +44,8 @@ free_map_allocate (size_t cnt, disk_sector_t *sectorp)
     }
   if (sector != BITMAP_ERROR)
     *sectorp = sector;
+
+  lock_release (&free_map_lock);
   return sector != BITMAP_ERROR;
 }
 
@@ -43,27 +53,37 @@ free_map_allocate (size_t cnt, disk_sector_t *sectorp)
 void
 free_map_release (disk_sector_t sector, size_t cnt)
 {
+  lock_acquire (&free_map_lock);
+
   ASSERT (bitmap_all (free_map, sector, cnt));
   bitmap_set_multiple (free_map, sector, cnt, false);
   bitmap_write (free_map, free_map_file);
+
+  lock_release (&free_map_lock);
 }
 
 /* Opens the free map file and reads it from disk. */
 void
 free_map_open (void) 
 {
+  lock_acquire (&free_map_lock);
+
   free_map_file = file_open (inode_open (FREE_MAP_SECTOR));
   if (free_map_file == NULL)
     PANIC ("can't open free map");
   if (!bitmap_read (free_map, free_map_file))
     PANIC ("can't read free map");
+
+  lock_release (&free_map_lock);
 }
 
 /* Writes the free map to disk and closes the free map file. */
 void
 free_map_close (void) 
 {
+  lock_acquire (&free_map_lock);
   file_close (free_map_file);
+  lock_release (&free_map_lock);
 }
 
 /* Creates a new free map file on disk and writes the free map to
@@ -71,6 +91,8 @@ free_map_close (void)
 void
 free_map_create (void) 
 {
+  lock_acquire (&free_map_lock);
+
   /* Create inode. */
   if (!inode_create (FREE_MAP_SECTOR, bitmap_file_size (free_map)))
     PANIC ("free map creation failed");
@@ -81,4 +103,6 @@ free_map_create (void)
     PANIC ("can't open free map");
   if (!bitmap_write (free_map, free_map_file))
     PANIC ("can't write free map");
+
+  lock_release (&free_map_lock);
 }
