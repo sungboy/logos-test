3fb83b0b906d816c2b100547a9d30b460910f26e
ad1392 <ad1392@2f210a3a-a0f4-11de-923e-0b953e4d95d6>
2009-11-20 06:23:25 +0000

    program test

---
+++ trunk/pintos/src/tests/vm/lru-logos.c
@@ -0,0 +1,42 @@
+/* Demonstrate that the page can be replaced.
+   This must succeed. */
+
+#include <string.h>
+#include "tests/lib.h"
+#include "tests/main.h"
+
+void
+test_main (void)
+{
+  char stack_obj2[4096];
+  memset (stack_obj2, 0, 1);
+  msg ("obj2: %x", (int)stack_obj2);
+
+  char stack_obj3[4096];
+  memset (stack_obj3, 0, 1);
+  msg ("obj3: %x", (int)stack_obj3);
+
+  memset (stack_obj2, 0, 1);
+
+  char stack_obj1[4096];
+  memset (stack_obj1, 0, 1);
+  msg ("obj1: %x", (int)stack_obj1);
+
+  // foo();
+
+  char stack_obj5[4096];
+  memset (stack_obj5, 0, 1);
+  msg ("obj5: %x", (int)stack_obj5);
+
+  memset (stack_obj2, 0, 1);
+
+  char stack_obj4[4096];
+  memset (stack_obj4, 0, 1);
+  msg ("obj4: %x", (int)stack_obj4);
+
+  memset (stack_obj5, 0, 1);
+  memset (stack_obj3, 0, 1);
+  memset (stack_obj2, 0, 1);
+  memset (stack_obj5, 0, 1);
+  memset (stack_obj2, 0, 1);
+}
